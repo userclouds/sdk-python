4e48bef47284a1a52fc9825a278914f9faf382ff
uc-build-user <116039615+uc-build-user@users.noreply.github.com>
2023-10-11 06:04:28 -0700

    Sync monorepo state at "Fix python lintfix script so it doesn't apply to the virutal env" (#25)
    Syncing from
    userclouds/userclouds@7f5998cfd91cc9cccfaea7fee3b5bb662ff4ccbe

--- lintfix.sh
+++ lintfix.sh
@@ -6,5 +6,5 @@ PYTHON_SDK_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # shellcheck disable=SC1091
 source "${PYTHON_SDK_DIR}/.venv/bin/activate"
 find "${PYTHON_SDK_DIR}/src" -print0 -type f -name "*.py" | xargs pyupgrade --py39-plus
-black "${PYTHON_SDK_DIR}"
-isort --profile black "${PYTHON_SDK_DIR}"
+black "${PYTHON_SDK_DIR}/src"
+isort --profile black "${PYTHON_SDK_DIR}/src"
